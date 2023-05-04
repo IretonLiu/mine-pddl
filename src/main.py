6eb590a4a9a113c649f3ff432e0fb06f6b3120b0
Ireton Liu <iretonsama@gmail.com>
2023-05-04 12:45:22 +0200

    inventory intialisation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-# import minedojo
-# import json_helper
-# import handlers.entities as handlers
-# import numpy as np
+import minedojo
+import json_helper
+import handlers.entities as handlers
+import numpy as np
 import pickle
 from pddl.pddl_types.named_pddl_types import NamedBlockType, NamedItemType
 from pddl.functions import *
@@ -14,29 +14,31 @@ from pddl.problem import Problem
 
 # todo: process the agent's inventory - will go into the items dict
 
-# world_json = json_helper.load_json("worlds/example.json")
-# print(world_json)
-# print(json_helper.json_blocks_to_xml_str(world_json["blocks"]))
+world_json = json_helper.load_json("worlds/example.json")
+print(world_json)
+print(json_helper.json_blocks_to_xml_str(world_json["blocks"]))
 
 
-# env = minedojo.make(
-#     "open-ended",
-#     image_size=(1024, 1024),
-#     world_seed="Enter the Nether",
-#     start_position=dict(x=-260, y=86, z=173, yaw=0, pitch=0),
-#     use_voxel=True,
-#     # spawn_mobs=False,
-#     voxel_size=dict(xmin=-4, ymin=-4, zmin=-4, xmax=4, ymax=4, zmax=4),
-#     drawing_str=f"""{json_helper.json_blocks_to_xml_str(world_json["blocks"])}"""
-# )
+env = minedojo.make(
+    "open-ended",
+    image_size=(1024, 1024),
+    world_seed="Enter the Nether",
+    start_position=dict(x=-260, y=86, z=173, yaw=0, pitch=0),
+    use_voxel=True,
+    # spawn_mobs=False,
+    voxel_size=dict(xmin=-4, ymin=-4, zmin=-4, xmax=4, ymax=4, zmax=4),
+    drawing_str=f"""{json_helper.json_blocks_to_xml_str(world_json["blocks"])}""",
+    initial_inventory=json_helper.json_inventory_to_inventory_item(world_json["inventory"]),
+)
 
-# ranges = (5, 5, 5)
-# env.env.env.env.env._sim_spec._obs_handlers.append(
-#     handlers.EntityObservation(ranges))
+ranges = (5, 5, 5)
+env.env.env.env.env._sim_spec._obs_handlers.append(
+    handlers.EntityObservation(ranges))
 
-# obs = env.reset()
-# voxels = obs['voxels']
-# entities = obs['entities']
+obs = env.reset()
+voxels = obs['voxels']
+entities = obs['entities']
+inventory = obs["inventory"]
 
 # done = False
 # while True:
