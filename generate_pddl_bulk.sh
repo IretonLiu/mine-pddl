25c71e44b287c77cf8f8b10030b92a95e24ea7ab
William Hill <whill4@me.com>
2023-12-11 23:44:06 +0200

    updated run scripts

--- generate_pddl_bulk.sh
+++ generate_pddl_bulk.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
-declare -a task_array=(
-    "Build_Log_Cabin" "Move_to_Location" "Place_Wood" "PDDL Files" "Cut_Tree" "Gather_Multiple_Wood" "Climb_Place" "Bridge" "Build_Well" 
-    "Farm_Wheat" "Craft_Item" "Build_Wall" "Pickup_and_Place" "Grow_Tree" "Collect_Build_Shape" "Gather_Wood" "Build_Shape" "Build_Cross" "Pickup_Diamond"
-)
+# define the tasks to skip
+declare -a skip_task_array=("PDDL Files" "Farm_Wheat" "Craft_Item" "Grow_Tree")
+
+# define the difficulty levels
 declare -a difficulty_array=("Easy" "Medium" "Hard")
+
+# define the default observation ranges, as well as any task-specific overrides
+# note that the overrides will have to specially handled later on
 declare -A obsrange_array=(
     [Easy]="(8, 4, 8)" 
     [Medium]="(15, 8, 15)" 
@@ -20,29 +23,39 @@ declare -A cuttree_obsrange_array=(
     [Medium]="(40, 30, 40)" 
     [Hard]="(65, 30, 65)"
 )
+
+# define the pddl types
 declare -a pddl_types=("numerical" "propositional")
 
-for task in ./task-worlds/worlds/*; do
+# loop through the tasks, difficulties, and pddl types
+for task in ./task-worlds/*; do
+    # extract the task name from the file path (it will be the last element)
     task=$(basename $task)
+
+    # check if the task is in the skip array
+    if [[ "${skip_task_array[@]}" =~ "${task}" ]]; then
+        echo "========== Skipping task ${task}... =========="
+        continue
+    fi
+
     echo "========== Processing task ${task} =========="
     for difficulty in ${difficulty_array[@]}; do 
         for pddl_type in ${pddl_types[@]}; do 
-
-
-            task_folder="./task-worlds/worlds/${task}/${task}_${difficulty}"
+            # define the task folder and yaml file
+            task_folder="./task-worlds/${task}/${task}_${difficulty}"
             yaml_file="${task_folder}.yaml"
 
+            # define the observation range, and check if there is a task-specific override
             obsrange=${obsrange_array[$difficulty]}
-
             if [ $task = "Build_Log_Cabin" ]; then
                 obsrange=${logcabin_obsrange_array[$difficulty]}
             elif [ $task = "Cut_Tree" ]; then
                 obsrange=${cuttree_obsrange_array[$difficulty]}
             fi
             
-            echo "Generating PDDL for ${task}_${difficulty} for pddl type ${pddl_type}..."
+            echo "Generating PDDL for ${task}_${difficulty} for ${pddl_type} pddl..."
 
-            # check if the yaml file exists
+            # check if the yaml file exists - there may be special cases where it does not
             if [ ! -f $yaml_file ]; then
                 echo "YAML file ${yaml_file} does not exist! Skipping..."
                 continue
--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 python src/main.py \
     --generate-pddl \
     --pddl-type propositional \
-    --world-config task-worlds/worlds/Place_Wood/Place_Wood_Easy.yaml \
+    --world-config task-worlds/Move_to_Location/Move_to_Location_Easy.yaml \
     --domain-name test_domain \
     --problem-name test_problem \
-    --domain-file problems/tasks/Build_Cross/Easy/propositional/d.pddl \
-    --problem-file problems/tasks/Build_Cross/Easy/propositional/p.pddl \
+    --domain-file problems/tasks/Move_to_Location/Easy/propositional/d.pddl \
+    --problem-file problems/tasks/Move_to_Location/Easy/propositional/p.pddl \
     --max-inventory-stack 5 \
-    --observation-range "(5, 4, 3)" \
-    --agent-start-position "(0, 5, 0)" 
+    --observation-range "(8, 4, 8)" 
